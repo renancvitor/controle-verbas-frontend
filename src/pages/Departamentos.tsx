f1d64f8022fbb26a2cc266aaa0bc09f85db3082b
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-17 22:46:31 -0300

    fix(departamento: corrige responsividade versão mobile)

--- src/pages/Departamentos.tsx
+++ src/pages/Departamentos.tsx
@@ -139,62 +139,63 @@ export default function Departamento() {
                         Cadastrar
                     </Button>
                 </div>
-
-                <table className="w-full border border-gray-700 rounded-lg">
-                    <thead>
-                        <tr className="bg-gray-800">
-                            <th className="border border-gray-700 p-2 text-left">Nome</th>
-                            <th className="border border-gray-700 p-2">Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {departamentos.map((departamento) => (
-                            <tr key={departamento.id} className="hover:bg-gray-800">
-                                <td className="border border-gray-700 p-2">
-                                    {editandoId === departamento.id ? (
-                                        <input
-                                            type="text"
-                                            value={nomeEditado}
-                                            onChange={(e) => setNomeEditado(e.target.value)}
-                                            className="w-full p-1 rounded bg-gray-800 text-white"
-                                        />
-                                    ) : (
-                                        departamento.nome
-                                    )}
-                                </td>
-                                <td className="border border-gray-700 p-2 text-center space-x-2">
-                                    {editandoId === departamento.id ? (
-                                        <>
-                                            <Button variant="success" onClick={() => salvarEdicao(departamento.id)}>
-                                                Salvar
-                                            </Button>
-                                            <Button variant="danger" onClick={cancelarEdicao}>
-                                                Cancelar
-                                            </Button>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <Button className="w-20" variant="primary" onClick={() => iniciarEdicao(departamento)}>
-                                                Editar
-                                            </Button>
-
-
-                                            {departamento.ativo ? (
-                                                <Button className="w-24" variant="danger" onClick={() => handleDeletar(departamento.id)}>
-                                                    Desativar
-                                                </Button>
-                                            ) : (
-                                                <Button className="w-24" variant="success" onClick={() => handleAtivar(departamento.id)}>
-                                                    Ativar
-                                                </Button>
-                                            )}
-                                        </>
-                                    )}
-                                </td>
+                <div className="overflow-x-auto w-full">
+                    <table className="w-full border border-gray-700 rounded-lg">
+                        <thead>
+                            <tr className="bg-gray-800">
+                                <th className="border border-gray-700 p-2 text-left">Nome</th>
+                                <th className="border border-gray-700 p-2">Ações</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {departamentos.map((departamento) => (
+                                <tr key={departamento.id} className="hover:bg-gray-800">
+                                    <td className="border border-gray-700 p-2">
+                                        {editandoId === departamento.id ? (
+                                            <input
+                                                type="text"
+                                                value={nomeEditado}
+                                                onChange={(e) => setNomeEditado(e.target.value)}
+                                                className="w-full p-1 rounded bg-gray-800 text-white"
+                                            />
+                                        ) : (
+                                            departamento.nome
+                                        )}
+                                    </td>
+                                    <td className="border border-gray-700 p-2 text-center space-x-2">
+                                        {editandoId === departamento.id ? (
+                                            <>
+                                                <Button variant="success" onClick={() => salvarEdicao(departamento.id)}>
+                                                    Salvar
+                                                </Button>
+                                                <Button variant="danger" onClick={cancelarEdicao}>
+                                                    Cancelar
+                                                </Button>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <div className="flex justify-end gap-2">
+                                                    <Button className="w-20" variant="primary" onClick={() => iniciarEdicao(departamento)}>
+                                                        Editar
+                                                    </Button>
+                                                    {departamento.ativo ? (
+                                                        <Button className="w-24" variant="danger" onClick={() => handleDeletar(departamento.id)}>
+                                                            Desativar
+                                                        </Button>
+                                                    ) : (
+                                                        <Button className="w-24" variant="success" onClick={() => handleAtivar(departamento.id)}>
+                                                            Ativar
+                                                        </Button>
+                                                    )}
+                                                </div>
+                                            </>
+                                        )}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
