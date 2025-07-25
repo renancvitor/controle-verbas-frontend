1ae94d01b41ec85dd6f8b6cd6f9f691c59abd7b6
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-25 12:24:06 -0300

    feat(usuario): adiciona botões com legenda ao invés de texto

--- src/components/usuarios/TabelaUsuarios.tsx
+++ src/components/usuarios/TabelaUsuarios.tsx
@@ -1,5 +1,11 @@
 import Button from "../ui/Button";
 
+import EditIcon from "@mui/icons-material/Edit";
+import BlockIcon from "@mui/icons-material/Block";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import Tooltip from "@mui/material/Tooltip";
+
+
 interface Usuario {
     id: number;
     email: string;
@@ -26,14 +32,14 @@ export default function TabelaUsuarios({
 }: Props) {
 
     return (
-        <div className="overflow-x-auto w-full">
-            <table className="w-full border border-gray-700 rounded-lg">
+        <div className="flex justify-center overflow-x-auto w-full">
+            <table className="table-fixed w-auto border border-gray-700 rounded-lg">
                 <thead>
                     <tr className="bg-gray-800">
 
-                        <th className="p-2 border w-1/4">Email</th>
-                        <th className="p-2 border w-1/4">Tipo</th>
-                        <th className="p-2 border w-1/4">Ações</th>
+                        <th className="p-2 border w-[300px]">Email</th>
+                        <th className="p-2 border w-[200px]">Tipo</th>
+                        <th className="p-2 border w-[180px]">Ações</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -41,38 +47,43 @@ export default function TabelaUsuarios({
                         <tr key={u.id} className="hover:bg-gray-800">
 
                             <td className="p-2 border">{u.email}</td>
-                            <td className="p-2 border">{u.tipoUsuario}</td>
+                            <td className="p-2 border text-center">{u.tipoUsuario}</td>
                             <td className="p-2 border">
+
                                 <div className="flex gap-2 justify-center">
-                                    <Button
-                                        className="w-24"
-                                        variant="primary"
-                                        onClick={() => {
-                                            onEditar(u);
-                                            abrirModal();
-                                        }}
-                                    >
-                                        Editar
-                                    </Button>
+                                    <Tooltip title="Editar" arrow>
+                                        <Button
+                                            variant="primary"
+                                            onClick={() => { onEditar(u); abrirModal(); }}
+                                            iconOnly
+                                        >
+                                            <EditIcon />
+                                        </Button>
+                                    </Tooltip>
 
                                     {u.ativo ? (
-                                        <Button
-                                            className="w-24"
-                                            variant="danger"
-                                            onClick={() => handleDesativar(u.id)}
-                                        >
-                                            Desativar
-                                        </Button>
+                                        <Tooltip title="Desativar" arrow>
+                                            <Button
+                                                variant="danger"
+                                                onClick={() => handleDesativar(u.id)}
+                                                iconOnly
+                                            >
+                                                <BlockIcon />
+                                            </Button>
+                                        </Tooltip>
                                     ) : (
-                                        <Button
-                                            className="w-24"
-                                            variant="success"
-                                            onClick={() => handleAtivar(u.id)}
-                                        >
-                                            Ativar
-                                        </Button>
+                                        <Tooltip title="Ativar" arrow>
+                                            <Button
+                                                variant="success"
+                                                onClick={() => handleAtivar(u.id)}
+                                                iconOnly
+                                            >
+                                                <CheckCircleIcon />
+                                            </Button>
+                                        </Tooltip>
                                     )}
                                 </div>
+
                             </td>
                         </tr>
                     ))}
